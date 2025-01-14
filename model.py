73e9e1be97d86f76120a18cccf9c48f426f71f27
lyc289 <2022201894@ruc.edu.cn>
2025-01-14 12:13:23 +0800

    first train

--- model.py
+++ model.py
@@ -1,44 +1,28 @@
-# coding: UTF-8
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import BertModel, BertTokenizer
 
-def process_dataset(dataset):
-    
-    return dataset
-
-
 class Config(object):
-
-    """配置参数"""
     def __init__(self, dataset):
-        self.model_name = 'bert'
-        self.train_path = dataset + '/data/train.txt'                                # 训练集
-        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
-        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.data_path = dataset 
         self.class_list = ['positive', 'negative', 'neutral']                               
-        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
-
+        self.save_path = 'saved_dict/bert.ckpt'
+        self.device = torch.device('cuda:1')  
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = 3                       
         self.num_epochs = 3                                             # epoch数
         self.batch_size = 128                                           # mini-batch大小
         self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-5                                       # 学习率
-        self.bert_path = './bert_pretrain'
+        self.bert_path = '/home/liuyichen/finbert-tone'
         self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
         self.hidden_size = 768
-        self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
-        self.num_filters = 256                                          # 卷积核数量(channels数)
         self.dropout = 0.1
         self.rnn_hidden = 256
         self.num_layers = 2
 
-
 class Model(nn.Module):
-
     def __init__(self, config):
         super(Model, self).__init__()
         self.bert = BertModel.from_pretrained(config.bert_path, return_dict=True)
---
+++ utils.py
@@ -0,0 +1,97 @@
+from tqdm import tqdm
+import torch
+import time
+from datetime import timedelta
+from sklearn.model_selection import train_test_split
+
+PAD, CLS = '[PAD]', '[CLS]'
+
+def build_dataset(config):
+    def load_dataset(path, pad_size=32):
+        contents = []
+        with open(path, 'r', encoding='UTF-8') as f:
+            for line in tqdm(f):
+                lin = line.strip()
+                if not lin:
+                    continue
+                content, label = lin.split('@')
+                token = config.tokenizer.tokenize(content)
+                token = [CLS] + token
+                seq_len = len(token)
+                mask = []
+                token_ids = config.tokenizer.convert_tokens_to_ids(token)
+                if pad_size:
+                    if len(token) < pad_size:
+                        mask = [1] * len(token_ids) + [0] * (pad_size - len(token))
+                        token_ids += ([0] * (pad_size - len(token)))
+                    else:
+                        mask = [1] * pad_size
+                        token_ids = token_ids[:pad_size]
+                        seq_len = pad_size
+                #LABEL_0: neutral; LABEL_1: positive; LABEL_2: negative
+                label2int = {'neutral': 0, 'positive': 1, 'negative': 2}[label]
+                contents.append((token_ids, label2int, seq_len, mask))
+        return contents
+    
+    data = load_dataset(config.data_path, config.pad_size)
+    train_data, temp_data = train_test_split(
+        data, test_size=0.2, random_state=42, stratify=[item[1] for item in data]
+    )
+    dev_data, test_data = train_test_split(
+        temp_data, test_size=0.5, random_state=42, stratify=[item[1] for item in temp_data]
+    )
+    return train_data, dev_data, test_data
+
+class DatasetIterater(object):
+    def __init__(self, batches, batch_size, device):
+        self.batch_size = batch_size
+        self.batches = batches
+        self.n_batches = len(batches) // batch_size
+        self.residue = False  # 记录batch数量是否为整数
+        if len(batches) % self.n_batches != 0:
+            self.residue = True
+        self.index = 0
+        self.device = device
+
+    def _to_tensor(self, datas):
+        x = torch.LongTensor([_[0] for _ in datas]).to(self.device)
+        y = torch.LongTensor([_[1] for _ in datas]).to(self.device)
+
+        # pad前的长度(超过pad_size的设为pad_size)
+        seq_len = torch.LongTensor([_[2] for _ in datas]).to(self.device)
+        mask = torch.LongTensor([_[3] for _ in datas]).to(self.device)
+        return (x, seq_len, mask), y
+
+    def __next__(self):
+        if self.residue and self.index == self.n_batches:
+            batches = self.batches[self.index * self.batch_size: len(self.batches)]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+        elif self.index >= self.n_batches:
+            self.index = 0
+            raise StopIteration
+        else:
+            batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        if self.residue:
+            return self.n_batches + 1
+        else:
+            return self.n_batches
+
+def build_iterator(dataset, config):
+    iter = DatasetIterater(dataset, config.batch_size, config.device)
+    return iter
+
+def get_time_dif(start_time):
+    end_time = time.time()
+    time_dif = end_time - start_time
+    return timedelta(seconds=int(round(time_dif)))
